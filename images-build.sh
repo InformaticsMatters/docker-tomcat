252edf19f90abe505350a8b69a4609c3077830ab
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2019-07-31 14:11:57 +0100

    added Java11 build

--- images-build.sh
+++ images-build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 docker build -f Dockerfile-tomcat85-jre8 -t informaticsmatters/tomcat:8.5-jre8 .
+docker build -f Dockerfile-tomcat85-jre11 -t informaticsmatters/tomcat:8.5-jre11 .
 docker build -f Dockerfile-tomcat85-jre8-with-content -t informaticsmatters/tomcat-with-content:8.5-jre8 .
 docker build -f Dockerfile-tomcat85-jre8-keycloak -t informaticsmatters/tomcat-keycloak:8.5-jre8-3.4.3 .
